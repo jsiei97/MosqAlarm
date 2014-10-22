9278fbd7c3c1c602710b2c079e2b6b808f67fb07
Johan Simonsson <github@fun-tech.se>
2014-10-22 21:44:43 +0200

    Add a MQTT interface

--- MosqAlarm.pro
+++ MosqAlarm.pro
@@ -1,12 +1,16 @@
 TEMPLATE = app
 QT += sql
+TARGET =
 
-TARGET = 
 DEPENDPATH  += src
 INCLUDEPATH += src
+LIBS += -lmosquittopp -lmosquitto
+
+target.path = /usr/local/bin/
+INSTALLS += target
 
 # Input
 SOURCES += src/main.cpp
 SOURCES += src/UnixTime.cpp
 SOURCES += src/SQLiteWrapper.cpp
-
+SOURCES += src/MosqConnect.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <QDebug>
 #include "SQLiteWrapper.h"
+#include "MosqConnect.h"
 
 int main()
 {
@@ -16,4 +17,24 @@ int main()
     qDebug() << "id rum1:" << lite.getId("rum1");
     qDebug() << "id rum2:" << lite.getId("rum2");
 
+       class MosqConnect *mqtt;
+        int rc;
+
+        mosqpp::lib_init();
+
+        mqtt = new MosqConnect(
+                "MosqAlarm",
+                "mosqhub",
+                1883
+                );
+
+        while(1)
+        {
+            rc = mqtt->loop();
+            if(rc)
+            {
+                mqtt->reconnect();
+            }
+        }
+        mosqpp::lib_cleanup();
 }
